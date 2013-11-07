695d9fa82a4a26b17c417df90b04315617e27b27
unknown <twissel@gmail.com>
2013-11-07 17:06:29 +0200

    up

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 require_once 'classes/Geo.php';
 require_once 'classes/TranzitNumber.php';
 
-error_reporting(E_ALL);
-ini_set('error_reporting', 'E_ALL');
-
 $c = isset($_GET['c']) ? $_GET['c'] : 'jsonp';
 
 $country = $_SERVER['HTTP_HOST'];
